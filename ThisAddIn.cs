a71c04b55b395572486c7a98178362e39bb23471
Gustavo Costa <gcscript.dev@gmail.com>
2022-02-18 19:20:53 -0300

    Adicionar arquivos de projeto.

---
+++ ThisAddIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace GCScript_for_Outlook
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Nota: o Outlook não aciona mais esse evento. Se você tiver o código que 
+            //    deve ser executado quando o Outlook é desligado, consulte https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region Código gerado por VSTO
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
